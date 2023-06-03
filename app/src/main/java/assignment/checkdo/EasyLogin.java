da52b676ddd88a76ce6788e89b260e8a749cd9e3
IsThereAnyCode <hjk7625@gmail.com>
2023-06-04 03:54:00 +0900

    CheckDo(ToDoList)

--- app/src/main/java/assignment/checkdo/EasyLogin.java
+++ app/src/main/java/assignment/checkdo/EasyLogin.java
@@ -15,12 +15,21 @@ public class EasyLogin extends AppCompatActivity {
         setContentView(R.layout.activity_easy_login);
 
         Button loginbtn = (Button)findViewById(R.id.login);
+        Button registerbtn = (Button)findViewById(R.id.gotoregister);
 
         loginbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivityForResult(intent, 1001);
+            }
+        });
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivityForResult(intent, 1002);
             }
         });
     }
---
+++ app/src/main/java/assignment/checkdo/RegisterActivity.java
@@ -0,0 +1,54 @@
+package assignment.checkdo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    private EditText mEtEmail, mEtPwd;      // 회원가입 입력필드
+    private Button mBtnRegister;            // 회원가입 버튼
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+        mBtnRegister = findViewById(R.id.finishregister);
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 회원가입 처리 시작
+                String strEmail = mEtEmail.getText().toString();    // 사용자가 입력한 이메일 값을 가져옴
+                String strPwd = mEtPwd.getText().toString();        // 사용자가 입력한 비밀번호 값을 가져옴
+
+                // Firebase Auth 진행
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    }
+                });
+            }
+        });
+    }
+}
