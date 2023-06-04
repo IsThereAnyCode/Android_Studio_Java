7fc490d602938da8691ebd1f1ea745b5c9d07e18
IsThereAnyCode <hjk7625@gmail.com>
2023-06-04 19:54:05 +0900

    CheckDo(ToDoList)

--- app/src/main/java/assignment/checkdo/EasyLogin.java
+++ app/src/main/java/assignment/checkdo/EasyLogin.java
@@ -1,35 +1,78 @@
 package assignment.checkdo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EasyLogin extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;     // 파이어베이스 인증
+    private DatabaseReference mDatabaseRef; // 실시간 데이터베이스
+    private EditText mEtEmail, mEtPwd;      // 로그인 입력필드
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_login);
 
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("CheckDo");
+
+        mEtEmail = findViewById(R.id.edit_email);
+        mEtPwd = findViewById(R.id.edit_password);
+
         Button loginbtn = (Button)findViewById(R.id.login);
         Button registerbtn = (Button)findViewById(R.id.gotoregister);
 
+
+
+        // 로그인 요청 버튼
         loginbtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivityForResult(intent, 1001);
+                String strEmail = mEtEmail.getText().toString();    // 사용자가 입력한 이메일 값을 가져옴
+                String strPwd = mEtPwd.getText().toString();        // 사용자가 입력한 비밀번호 값을 가져옴
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(EasyLogin.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            // 로그인 성공!!
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(EasyLogin.this, "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+                            finish();   // 현재 액티비티 파괴
+                        }
+                        else {
+                            Toast.makeText(EasyLogin.this, "!!로그인 실패!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
             }
         });
 
+
+        // 회원가입 창으로 이동
         registerbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivityForResult(intent, 1002);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/assignment/checkdo/RegisterActivity.java
+++ app/src/main/java/assignment/checkdo/RegisterActivity.java
@@ -3,15 +3,18 @@ package assignment.checkdo;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -28,7 +31,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
-        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("CheckDo");
 
         mEtEmail = findViewById(R.id.et_email);
         mEtPwd = findViewById(R.id.et_pwd);
@@ -45,7 +48,24 @@ public class RegisterActivity extends AppCompatActivity {
                 mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                            UserAccount account = new UserAccount();
+                            account.setIdToken(firebaseUser.getUid());      // 고유값 가져와 account의 setIdToken에 할당
+                            account.setEmailId(firebaseUser.getEmail());    // 이메일 가져와 account의 setEmailId에 할당
+                            account.setPassword(strPwd);                    // 비밀번호 가져와 account의 setPassword에 할당
 
+                            // setValue : database에 insert(삽입) 행위
+                            mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+                            
+                            // 메시지 띄우며 EasyLogin으로 이동
+                            Toast.makeText(RegisterActivity.this, "회원가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), EasyLogin.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this, "회원가입에 실패하셨습니다.", Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
             }
